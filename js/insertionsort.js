907fc1a27bc7b9d20c201ce8d78dd1eb69b16f73
karthik chiru <karthikchiru12@gmail.com>
2019-02-26 15:24:44 +0530

    changes to insertion sort

--- js/insertionsort.js
+++ js/insertionsort.js
@@ -32,16 +32,14 @@ var temp;//used as a temporary variable//
 var x;//extra//
 var i=0,j=1;
 var pass=0;
-var nswap=0;
-var eswap=0;
+var swap=0;
 var n=11;
 var step1="enter";
 var step2="";
 var step3="";
 var left,right;
 var s;
-var nnext=0;
-var enext=0;
+var next=0;
 var pc=0;
 var str;
 var finish=0;
@@ -49,32 +47,42 @@ var swap_count=0;
 var sort=0;
 var comp=0;
 var exp=0;
+var k,l=0;
 
 
 function play_asc()
 {
+    k=i;
+    l=j;
     var id=setInterval(anm,1000);
     function anm()
     {
 
 
-        if(nnext==1)
+
+        if(next==1)
         {
-        arr.rows[r].cells[i].style.backgroundColor="";
-        arr.rows[r].cells[j].style.backgroundColor="";
-        arr.rows[2].cells[i].innerHTML="";
-        arr.rows[2].cells[j].innerHTML="";
-        left=0;
-        right=0;
-        temp=0;
-        j++;
-        nnext=0;
-        document.get
-        step1="enter"; 
+            i=0;
+            j++;
+            step1="enter";
+            arr.rows[r].cells[i].style.backgroundColor="";
+            arr.rows[r].cells[j].style.backgroundColor="";
+            arr.rows[r+1].cells[i].innerHTML="";
+            arr.rows[r+1].cells[j].innerHTML="";
         }
 
-        if(nswap==1)
+
+
+
+        if(swap==1)
         { 
+        document.getElementById('line3').style.backgroundColor="";
+        document.getElementById('line4').style.backgroundColor="red";
+        msg="";
+        document.getElementById('swap').innerHTML="";
+        msg="swapped :"+left+" & "+right+" ;";
+        document.getElementById('swap').innerHTML=msg;
+        msg="";
         //arr.rows[2].cells[i].innerHTML="swapped";
         temp=left;
         left=right;
@@ -82,101 +90,111 @@ function play_asc()
         //arr.rows[2].cells[j].innerHTML=""+left+"&"+right;
         arr.rows[r].cells[i].innerHTML=left;
         arr.rows[r].cells[j].innerHTML=right;
-        nswap=0;
+        swap=0;
         next=1;
+        swap_count++;
+        document.getElementById('swap_count').innerHTML="Number of swaps : "+swap_count;
 
         }
-
-
-
-
-
-
-
-
-
-
-
-        if(step1=="continue")
-        {
-            step1="";
-            step2="enter";
-            console.log("hello");
-        }
-        if(step1=="enter")
-        {
-            left=Number(arr.rows[r].cells[i].innerHTML);
-            arr.rows[r].cells[i].style.backgroundColor="red";
-            right=Number(arr.rows[r].cells[j].innerHTML);
-            arr.rows[r].cells[j].style.backgroundColor="green";
-
-            step1="continue";
-        }
-
-
-        if(step2=="clear")
-        {
-
-            step1="enter";
-
-        }
-        if(step2="expand")
-        {
-            j++;
-            exp=1;
-            step2="";
-            step2="clear";
-        }
-        if(step2=="continue")
-        {
+     
+      if(step1=="continue")
+      {
+          step1="";
+          step2="enter";
+      }
+      if(step1=="enter")
+      {
+            if(exp==2)
+            {
+                i++;
+                arr.rows[r].cells[i].style.backgroundColor="";
+                arr.rows[r].cells[j].style.backgroundColor="";
+                
+            }
             if(exp==1)
             {
-                stepex="enter";
-                step2="";
-            }
-            else
-            {
-              step3="enter";
-              step2="";
-            }
-        }
-        if(step2=="enter")
-        {
+                j++;
+                i=0;
+                arr.rows[r].cells[i].style.backgroundColor="";
+                arr.rows[r].cells[j].style.backgroundColor="";
             
-            if(j<n)
-            {  
+            }
+            left=Number(arr.rows[r].cells[i].innerHTML);
+            right=Number(arr.rows[r].cells[j].innerHTML);
+            arr.rows[r].cells[i].style.backgroundColor="green";
+            arr.rows[r].cells[j].style.backgroundColor="red";
+            step1="continue";
+          
+      }
 
-                    if(right<left)
-                    { 
-                          
-                          temp=left;
-                          arr.rows[r].cells[j].innerHTML="";
-                          arr.rows[r].cells[j].style.backgroundColor="";
-                          arr.rows[r+1].cells[j].style.visibility="visible";
-                          arr.rows[r+1].cells[j].innerHTML=temp;
-                          step2="continue";
-  
-
-                    }
-                    else
+      if(step2=="continue")
+      {
+          step2="";
+          step3="enter";
+      }
+      if(step2=="enter")
+      {
+          if(i<j)
+          {
+              
+                  if(right<left)
+                  {
+                    temp=right;
+                    arr.rows[2].cells[j].innerHTML=temp;
+                    arr.rows[r].cells[i].style.backgroundColor="black";
+                    arr.rows[r].cells[j].style.backgroundColor="black";
+                    arr.rows[r].cells[j].innerHTML="";
+                    comp++;
+                    if(exp!=1&&exp!=2)
                     {
-                        step2="expand";
+                     step2="continue";
+                    }
+                    if(exp==1)
+                    {
+                        stepex="enter";
                     }
 
-            }
-        }
+                  }
+                  else
+                  {
+                      if(exp==1)
+                      {
+                          exp=2;
+                          step2="";
+                          step1="enter";
+                      }
+                      else
+                      {
+                          exp=1;
+                          step2="";
+                          step1="enter";
+                      }
+
+                  }
+          }
+          else
+          {
+              exp=1;
+
+          }
+      }
+
+      if(step3=="enter")
+	 {
+	  s=right;
+	  arr.rows[2].cells[j].innerHTML="";
+	  arr.rows[2].cells[i].innerHTML=s;
+	  swap=1;
+	  s=0;
+	  step3="";
+	 }
 
 
 
-        if(step3=="enter")
-        {
-            
-            s=right;
-	        arr.rows[2].cells[j].innerHTML="";
-	        arr.rows[2].cells[i].innerHTML=s;
-	        nswap=1;
-	        s=0;
-	        step3="";
-        }
+
+
+
+
+        
     }
 }
