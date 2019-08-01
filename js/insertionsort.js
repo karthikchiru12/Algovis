c0343ff0e74d2b8f99def4233922bbef1081e38b
Chiranjeevi Karthik <karthikchiru12@gmail.com>
2019-08-01 13:25:03 +0530

    trace corrected in selection sort and also under development tag is added to the algorithms

--- js/insertionsort.js
+++ js/insertionsort.js
@@ -1,35 +1,52 @@
+/*MIT License
+
+Copyright (c) 2018 Karthik Chiranjeevi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+Developed by karthikchiru12 
+*/
 var arr = document.getElementById('array_1d');//declaration used to access the 1-d array in the <body>//
 var col_len=arr.rows[0].cells.length;//row length//
 var row_len=arr.rows.length;//col length//
 var r=1;c=0;//initial row and col values where row value in linear search remains constant only co value changes//
 var status; //possible values={pl(play()),done(either element found or reset button pressed while playing),""(initial)}//
-var fst=1;//it is a flag used to describe first element is being searched or not {1(false)_initial,0(true)}//
 var msg="";//used to display current key value to the user//
 var temp;//used as a temporary variable//
 var x;//extra//
-var i=0,j=1;
-var pass=0;
-var swap=0;
+var i=0,j=0,curr=0,timer=1000,p=0,q=0;
 var n=11;
 var step1="enter";
 var step2="";
 var step3="";
-var step4="";
-var left,right;
 var s;
-var next=0;
-var pc=0;
 var str;
 var finish=0;
 var swap_count=0;
 var sort=0;
-var comp=0;
-var k=1;
+var a=[];
+var min=0;
+var min_index=0;
 
 //reset function
 function reset()
 {
-    if(status=="done"&&(finish==1||finish==0)&&status!="pl")
+    if((status=="done"||status=="")&&(finish==1||finish==0)&&status!="pl")
     {
 	 /* The Limitation of the reset function is that it cannot be invoked while
 	    the anim() is running as it interferes with setInterval() and produces 
@@ -37,30 +54,26 @@ function reset()
      c=0;
      msg="";
      finish=0;
-     left=0;
-     right=0;
-     check=1;
      step1="enter";
 	 step2="";
 	 step3="";
-	 step4=""
-     pass=0;
-	 swap=0;
-	 next=0;
-	 pc=0;
 	 swap_count=0;
 	 temp=0;
 	 sort=0;
-	 comp=0;
-     document.getElementById('pass').innerHTML="";
-     document.getElementById('swap').innerHTML="";
+     a=0;
+     b=0;
+     min=0;
+     min_index=0;
+     a=[];
+     curr=0;
+     p=0;
+     q=0;
      document.getElementById('swap_count').innerHTML="";
-     document.getElementById('comp').innerHTML="";
-	 document.getElementById('line7').style.backgroundColor="";
-     for(var temp=0;temp<11;temp++)
+     document.getElementById('swap').innerHTML="";
+     for(let p=0;p<11;p++)
      {
-      arr.rows[1].cells[temp].style.backgroundColor="";
-	  arr.rows[2].cells[temp].innerHTML="";
+      arr.rows[1].cells[p].style.backgroundColor="";
+	  arr.rows[2].cells[p].innerHTML="";
      }
 	 
 	 arr.rows[r].cells[0].innerHTML="56";
@@ -73,7 +86,13 @@ function reset()
 	 arr.rows[r].cells[7].innerHTML="98";
 	 arr.rows[r].cells[8].innerHTML="124";
 	 arr.rows[r].cells[9].innerHTML="46";
-	 arr.rows[1].cells[10].innerHTML="35";
+     arr.rows[1].cells[10].innerHTML="35";
+     
+     document.getElementById('line4').style.backgroundColor='';
+     document.getElementById('line6').style.backgroundColor='';
+     document.getElementById('line8').style.backgroundColor='';
+     document.getElementById('line10').style.backgroundColor='';
+     document.getElementById('sort').style.visibility='visible';
 	 
      status="";
 	 if(s==true)
@@ -83,6 +102,7 @@ function reset()
 	 }
 	 
      }
+    
 }
 //reset function
  
@@ -91,275 +111,102 @@ function play_asc()
  
 
   
-  var id = setInterval(anm,500);//we repeat anim() function for every 2500ms until clearInterval(id) is called//
+  var id = setInterval(anm,1000);//we repeat anim() function for every 2500ms until clearInterval(id) is called//
   function anm()
   {
-   
-   if(sort==1)
-   {
-    alert("The array is already sorted, please refresh to sort a new array...!");
-	clearInterval(id);
-   }
-   
-   if(sort!=1)
-   {
-    
-    if(finish==1&&status=="done")
-	{
-	 s=true;
-	 reset();
-	 clearInterval(id);
-	 
-	}
-    status="pl";
-    if(k<=10)
-	{
-	
-	 
-	 if(next==1)
-	 {
-		 
-	  arr.rows[r].cells[i].style.backgroundColor="";
-	  arr.rows[r].cells[j].style.backgroundColor="";
-	  arr.rows[2].cells[i].innerHTML="";
-	  arr.rows[2].cells[j].innerHTML="";
-	  left=0;
-	  right=0;
-	  temp=0;
-		i=0;
-		k++;
-	  j=k;
-	  next=0;
-	  document.get
-		step1="enter"; 
-		for(var n=i;n<j;n++)
-		{
-			arr.rows[r].cells[n].style.backgroundColor="";
-		}
-	  msg="";
-	  document.getElementById('comp').innerHTML="";
-	  msg="No of comparisions : "+comp;
-	  document.getElementById('comp').innerHTML=msg;
-		msg="";
-		document.getElementById('swap_count').innerHTML="Number of swaps : "+swap_count;
-	  if(j<=10)
-	  {
-	  step1="";
-	  pc=1;
-	  }
-	  
-	 
-	 }
-	 if(pc==1)
-	 {
-	  document.getElementById('line3').style.backgroundColor="";
-	  document.getElementById('line4').style.backgroundColor="";
-	  arr.rows[r].cells[i].style.backgroundColor="";
-	  arr.rows[r].cells[j].style.backgroundColor="";
-	  arr.rows[2].cells[i].innerHTML="";
-	  arr.rows[2].cells[j].innerHTML="";
-	  left=0;
-	  right=0;
-	  temp=0;
-	  i=0;
-	  next=0;
-	  swap=0;
-	  pc=0;
-	  step1="enter";
-	 }
-	 if(swap==1)
-	 {swap_count++;
-		document.getElementById('line3').style.backgroundColor="";
-		document.getElementById('line4').style.backgroundColor="red";
-		document.getElementById('swap').innerHTML="";
-	  //msg="swapped :"+left+" & "+right+" ;";
-		//document.getElementById('swap').innerHTML=msg;
-		temp=left;
-	  left=right;
-	  right=temp;
-	  //arr.rows[2].cells[j].innerHTML=""+left+"&"+right;
-	  arr.rows[r].cells[i].innerHTML=left;
-		arr.rows[r].cells[j].innerHTML=right;
-		swap=0;
-		next=1;
-		document.getElementById('swap_count').innerHTML="Number of swaps : "+swap_count;
-	 }
-	 else if(swap>1){
-		 swap_count++;
-		 for(var n=i;n<=j;n++){
-			arr.rows[r].cells[n].style.backgroundColor="#FF0000";
-		 }
-		
-		 document.getElementById('swap_count').innerHTML="Number of swaps : "+swap_count;
-			arr.rows[r].cells[j].innerHTML=Number(arr.rows[r].cells[j-1].innerHTML);
-			arr.rows[2].cells[j].innerHTML="";
-			arr.rows[r].cells[j].style.backgroundColor="";
-			arr.rows[r].cells[j-1].style.backgroundColor="black";
-			arr.rows[r].cells[j-1].innerHTML="";
-			j--;
-			if(i+1==j)
-			{
-				step4="enter";
-			}
-			else{
-			swap=j-i;
-			}
-		
-	 }
-	
-	 if(step1=="complete")
-	 {
-		step1="";
-		step2="enter";
-	 }
-	 if(step1=="enter")
-	 {
-		msg="";
-	  document.getElementById('comp').innerHTML="";
-	  msg="No of comparisions : "+comp;
-	  document.getElementById('comp').innerHTML=msg;
-		msg="";
-		document.getElementById('swap_count').innerHTML="Number of swaps : "+swap_count;
-		document.getElementById('sort').style.visibility="hidden";
-		if(j<=10)
-		arr.rows[r].cells[j].style.backgroundColor="green";
-		/*alert(i)
-		*/
-		arr.rows[r].cells[i].style.backgroundColor="red";
-		
-		step1="complete";
-		if(i==n&&j>n)
-	  {
-	   pc=1;
-	   step3="";
-	   step1="";
-	   step2="";
-	  }
-	 }
-	 if(step2=="abort")
-	 {arr.rows[r].cells[i].style.backgroundColor="";
-		 i++;
-		 if(i==j){
-			 step2="";
-			 next=1;
-		 }
-	 else{
-	step2="";
-	step1="enter";
-	 }
-	}
-	 
-	 
-	 
-	 if(step2=="complete")
-	 {step2="";
-	 
-	 step3="enter";
-	 
-	 }
-	 if(step2=="enter")
-	 {document.getElementById('line3').style.backgroundColor="red";
-	 document.getElementById('line4').style.backgroundColor="";
-	 left=Number(arr.rows[r].cells[i].innerHTML);
-	 right=Number(arr.rows[r].cells[j].innerHTML);
-	 comp++;
-	 if(left>right)
-	 {
-		
-		temp=right;
-		arr.rows[2].cells[j].innerHTML=temp;
-		arr.rows[r].cells[i].style.backgroundColor="red";
-		arr.rows[r].cells[j].style.backgroundColor="black";
-		arr.rows[r].cells[j].innerHTML="";
-		step2="complete";
-		swap=j-i;
-		
-	}
-	else{
-		step2="abort";
+      
+        if(sort==1)
+        {
+            alert("The array is already sorted, please refresh to sort a new array...!");
+            clearInterval(id);
+        }
+        
+        if(sort!=1)
+        {
+            
+            if(finish==1&&status=="done")
+            {
+            s=true;
+            reset();
+            clearInterval(id);
+            
+            }
+            status="pl";
+            
+            
+        
 
-	}
-	 
-	 }
-	 
-	if(step4=="enter"){
-		step4="";
-		swap=1;
+        
 
-	}
-	 
-	 
-	if(step3=="enter")
-	 {
-	  s=right;
-	  arr.rows[2].cells[j].innerHTML="";
-	  arr.rows[2].cells[i].innerHTML=s;
-	  swap=j-i;
-	  s=0;
-	  step3="";
-	 }
-	 
-	
-	 
-	 
-	 
-	 
-	}
-	
-	if(k==11)
-	{
-	
-	
-	  arr.rows[r].cells[i].style.backgroundColor="";
-	  finish=1;
-	  status="done";
-	  document.getElementById('line3').style.backgroundColor="";
-	  document.getElementById('line4').style.backgroundColor="";
-	  document.getElementById('line7').style.backgroundColor="green";
-	  sort=1;
-	  document.getElementById('sort').style.visibility="visible";
-	  
-	  clearInterval(id);
-	
-	}
-  }
+
+       
+
+        if(step1=="complete")
+        {       
+			
+                
+        }
+        if(step1=="enter")
+        {
+            
+           step1="complete";
+            
+        }
+
+        
+        
+
+    }
+
   }
 }
-  
+//refresh function//
 function refresh()
 {
  if((finish==0&&status!="pl")||(finish==1&&status=="done"))
  {
-  c=0;
+     
+     c=0;
      msg="";
      finish=0;
-     left=0;
-     right=0;
-     check=1;
      step1="enter";
 	 step2="";
 	 step3="";
-     pass=0;
-	 swap=0;
-	 next=0;
-	 pc=0;
 	 swap_count=0;
 	 temp=0;
 	 sort=0;
-	 comp=0;
-	 k=1;
-	 j=k;
-	 i=0;
-  document.getElementById('pass').innerHTML="";
-  document.getElementById('swap').innerHTML="";
-  document.getElementById('comp').innerHTML="";
-  document.getElementById('swap_count').innerHTML="";
-  document.getElementById('line7').style.backgroundColor="";
+     a=0;
+     b=0;
+     min=0;
+     min_index=0;
+     a=[];
+     curr=0;
+     p=0;
+     q=0;
+     status="";
+     document.getElementById('swap').innerHTML="";
+     document.getElementById('swap_count').innerHTML="";
+     document.getElementById('line4').style.backgroundColor='';
+     document.getElementById('line6').style.backgroundColor='';
+     document.getElementById('line8').style.backgroundColor='';
+     document.getElementById('line10').style.backgroundColor='';
+     document.getElementById('sort').style.visibility='visible';
   for(var i=0;i<col_len;i++)
   {
    x=Math.floor((Math.random() * 100) + 1);//random value is generated between 0 and 1 *100 +1//
    arr.rows[r].cells[i].innerHTML=x;
+   arr.rows[r].cells[i].style.backgroundColor='';
   }
  }
 }
 //refresh function end...
+
+//pause function
+function pause()
+{
+    if(status=='pl')
+    {
+        confirm('Visualization Paused...Press OK or Cancel to Resume');
+    }
+}
+//pause function
